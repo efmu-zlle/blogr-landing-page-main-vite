38a9ac152bacf6f707f9858a17d42cfa16538c86
Elmer Francisco M <74124971+efmu-zlle@users.noreply.github.com>
2023-06-19 21:50:34 -0600

    adding initial stuff

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../styles/components/header.css";
+// // here the rest of the components
+import "../styles/utils.css";
+import "../styles/breakpoints.css";
