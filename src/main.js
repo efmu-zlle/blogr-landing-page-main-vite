ff37c8b5c59fd5f83f030e9931b46ab03114218f
Elmer Francisco M <74124971+efmu-zlle@users.noreply.github.com>
2023-06-24 18:46:56 -0600

    Update html/css styling footer section (desktop)

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 import '../styles/styles.css';
 import '../styles/utils.css';
+
+// const heading = document.querySelector('.heading');
+
+// function animate() {
+//   heading.classList.add('active');
+// }
+
+// document.addEventListener('DOMContentLoaded', animate);
+
+// IntersectionObserver
