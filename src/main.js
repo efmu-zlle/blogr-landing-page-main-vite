2d851d3c96769d4c8d9ac3f7e537eb37e31031f9
Elmer Francisco M <74124971+efmu-zlle@users.noreply.github.com>
2023-07-06 20:47:16 -0600

    Refactor: navbar links

--- src/main.js
+++ src/main.js
@@ -20,3 +20,36 @@ function animation() {
 }
 
 window.addEventListener('DOMContentLoaded', animation);
+
+const modals = [...document.querySelectorAll('#modal')];
+const icons = [...document.querySelectorAll('#icon')];
+
+icons.forEach((btnIcon, index) =>
+  btnIcon.addEventListener('click', () => {
+    modals.forEach((modal) => {
+      if (modal !== modals[index]) {
+        modal.dataset.link = 'false';
+      }
+    });
+
+    icons.forEach((item) => {
+      if (item !== btnIcon) {
+        item.dataset.icon = 'false';
+      }
+    });
+
+    modals[index].dataset.link = 'true';
+    btnIcon.dataset.icon = 'true';
+  })
+);
+
+document.addEventListener('click', (e) => {
+  if (e.target.id == 'icon') return;
+  if (e.target.id == 'modal') return;
+  if (e.target.parentElement.id == 'modal') return;
+
+  icons.forEach((btnIcon, index) => {
+    modals[index].dataset.link = 'false';
+    btnIcon.dataset.icon = 'false';
+  });
+});
